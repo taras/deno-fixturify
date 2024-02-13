acb3750e2ce5049a273df9d97288a53492576cd7
Taras Mankovski <taras@frontside.com>
2024-02-13 16:12:02 -0500

    Fixed formatting errors

--- test_helpers.ts
+++ test_helpers.ts
@@ -1,6 +1,6 @@
 export async function removeTestDir() {
-    const info = await Deno.stat("testdir.tmp");
-    if (info.isDirectory) {
-      await Deno.remove("testdir.tmp", { recursive: true });
-    }
+  const info = await Deno.stat("testdir.tmp");
+  if (info.isDirectory) {
+    await Deno.remove("testdir.tmp", { recursive: true });
+  }
 }
